e9bf97cf91e4af57147b30cb1f529a909bf9c1e8
Reillyhewitson <reilly.hewitson@stfc.ac.uk>
2023-04-28 15:40:24 +0100

    Test redirecting to access keys

--- objectstore_interface/pages/access_key_pages/create.py
+++ objectstore_interface/pages/access_key_pages/create.py
@@ -38,5 +38,6 @@ async def create_object_store_keys(request: Request, storename, expires: Annotat
             }
             request.session['created'] = created
       time.sleep(0.5)
-      return RedirectResponse(f"/object-store/{storename}/create-keys", status_code=303)
+      return RedirectResponse(f"/object-store/{storename}/access-keys", status_code=303)
+      #return RedirectResponse(f"/object-store/{storename}/create-keys", status_code=303)
       #return templates.TemplateResponse("access_key_pages/keycreate.html", {"request": request, "storename": storename, "view": "create", "created": True}, status_code=201)
