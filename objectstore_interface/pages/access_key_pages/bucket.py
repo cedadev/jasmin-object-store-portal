9ad65ed197d9fd2d59f91e2f564620080580f4ca
Reillyhewitson <reilly.hewitson@stfc.ac.uk>
2023-05-24 11:26:17 +0100

    Add template picker

--- objectstore_interface/pages/access_key_pages/bucket.py
+++ objectstore_interface/pages/access_key_pages/bucket.py
@@ -81,3 +81,77 @@ async def create_permissions(
             exc_type, exc_value, exc_traceback = sys.exc_info()
             logging.error("".join(traceback.format_exception(etype=exc_type, value=exc_value, tb=exc_traceback)))
             return templates.TemplateResponse("error.html", {"request": request, "error": "".join(traceback.format_exception(etype=exc_type, value=exc_value, tb=exc_traceback))})
+    
+@router.post("/object-store/{storename}/buckets/{bucket}/templates")
+async def template_permissions(
+     request: Request,
+     storename, bucket,
+     template: Annotated[str, Form()]
+):
+    try:
+        object_store: ObjectStore = storefromjson(request.session[storename])
+        if template == "read-only-all":
+            response = await object_store.create_policy(
+                 actions="GetObject", 
+                 groups="null", 
+                 users="null", 
+                 application="Anonymous", 
+                 bucket=bucket, 
+                 name="Read-only access for Everyone",
+                 direction="Allow")
+        if template == "read-only-users":
+             response = await object_store.create_policy(
+                  actions="GetObject",
+                  groups="null",
+                  users="null",
+                  application="All",
+                  bucket=bucket,
+                  name="Read-only access for Users",
+                  direction="Allow"
+             )
+        if template == "full-access-users":
+            response = await object_store.create_policy(
+                 actions="*",
+                 groups="null",
+                 users="null",
+                 direction="Allow",
+                 application="All",
+                 bucket=bucket,
+                 name="Full access for Users",
+            )
+        if template == "uploads-no-login":
+             response = await object_store.create_policy(
+                  actions="CopyObject,CreateObject,PutObject",
+                  groups="null",
+                  users="null",
+                  application="Anonymous",
+                  bucket=bucket,
+                  name="Allow bucket uploads without login",
+                  direction="Allow"
+             )
+        if template == "no-uploads-no-login":
+             response = await object_store.create_policy(
+                  actions="*",
+                  groups="null",
+                  users="null",
+                  application="Anonymous",
+                  bucket=bucket,
+                  name="Prevent bucket uploads without login",
+                  direction="Deny"
+             )
+        if template == "bucket-manage-users":
+             response = await object_store.create_policy(
+                  actions="CopyBucket,CreateBucket,DeleteBucket,GetBucket,ListBucket",
+                  groups="null",
+                  users="null",
+                  application="All",
+                  bucket=bucket,
+                  name = "Grant bucket management to Users",
+                  direction="Allow"
+             )
+        
+        return RedirectResponse(f"/object-store/{storename}/buckets/{bucket}/policy", status_code=303)
+    except Exception:
+        exc_type, exc_value, exc_traceback = sys.exc_info()
+        logging.error("".join(traceback.format_exception(etype=exc_type, value=exc_value, tb=exc_traceback)))
+        return templates.TemplateResponse("error.html", {"request": request, "error": "".join(traceback.format_exception(etype=exc_type, value=exc_value, tb=exc_traceback))})
